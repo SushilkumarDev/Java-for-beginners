01918c77dd58535246da5a5760930e535360fdb9
SushiulkumarDev <sushilkumardeveloper@gmail.com>
2024-04-04 22:56:35 +0530

    basic code for MotorBike

---
+++ 05-IntroductionToObjectOrientedProgramming/src/com/in28minutes/oops/MotorBike.java
@@ -0,0 +1,17 @@
+package com.in28minutes.oops;
+
+public class MotorBike {
+	//state
+	private int speed; //member variable
+	
+
+	//constructors
+	MotorBike() {
+		this(5);
+	}
+	
+	MotorBike(int speed) {
+		this.speed = speed;
+	}
+}
+}
