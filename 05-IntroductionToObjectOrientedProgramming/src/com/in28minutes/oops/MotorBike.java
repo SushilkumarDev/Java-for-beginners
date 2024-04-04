816869a9f91d5c85538f67be6374f13faa2d18d6
SushiulkumarDev <sushilkumardeveloper@gmail.com>
2024-04-04 22:58:51 +0530

    basic code for MotorBike was successfully completed

--- 05-IntroductionToObjectOrientedProgramming/src/com/in28minutes/oops/MotorBike.java
+++ 05-IntroductionToObjectOrientedProgramming/src/com/in28minutes/oops/MotorBike.java
@@ -13,5 +13,25 @@ public class MotorBike {
 	MotorBike(int speed) {
 		this.speed = speed;
 	}
-}
+    //behavior
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		if(speed > 0 )
+			this.speed = speed;
+	}
+
+	public void increaseSpeed(int howMuch) {
+		setSpeed(this.speed + howMuch);
+	}
+
+	public void decreaseSpeed(int howMuch) {
+		setSpeed(this.speed - howMuch);
+	}
+	
+	void start() {
+		System.out.println("Bike Started");
+    }
 }
