eb684133799290bb6d284767968f33912e9429a3
SushilkumarDev <sushilkumardeveloper@gmail.com>
2024-04-18 22:58:53 +0530

    Added sumUptoN() and sumOfDivisors() methods to MyNumber class

--- 08-Loops/src/com/in28minutes/loop/MyNumber.java
+++ 08-Loops/src/com/in28minutes/loop/MyNumber.java
@@ -24,3 +24,27 @@ public class MyNumber {
 
 		return true;
 	}
+	public int sumUptoN() {
+		int sum = 0;
+
+		for (int i = 1; i <= number; i++) {
+			sum = sum + i;
+		}
+
+		return sum;
+	}
+
+	public int sumOfDivisors() {
+		// 6 except 1 , 6 => 2,3
+		// 2 + 3 + 4 + 5
+
+		int sum = 0;
+
+		for (int i = 2; i <= number - 1; i++) {
+			if (number % i == 0) {
+				sum = sum + i;
+			}
+		}
+
+		return sum;
+	}
