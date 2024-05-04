cc26393cf80529a7f4658fac16e9000adce91d76
SushilkumarDev <sushilkumardeveloper@gmail.com>
2024-05-04 17:51:23 +0530

    Add reveal-config.js file with configuration settings

--- 12-Collections/js/reveal.js
+++ 12-Collections/js/reveal.js
@@ -19,3 +19,48 @@
 		// Browser globals.
 		root.Reveal = factory();
 	}
+}( this, function() {
+
+	'use strict';
+
+	var Reveal;
+
+	// The reveal.js version
+	var VERSION = '3.5.0';
+
+	var SLIDES_SELECTOR = '.slides section',
+		HORIZONTAL_SLIDES_SELECTOR = '.slides>section',
+		VERTICAL_SLIDES_SELECTOR = '.slides>section.present>section',
+		HOME_SLIDE_SELECTOR = '.slides>section:first-of-type',
+		UA = navigator.userAgent,
+
+		// Configuration defaults, can be overridden at initialization time
+		config = {
+
+			// The "normal" size of the presentation, aspect ratio will be preserved
+			// when the presentation is scaled to fit different resolutions
+			width: 960,
+			height: 700,
+
+			// Factor of the display size that should remain empty around the content
+			margin: 0.04,
+
+			// Bounds for smallest/largest possible scale to apply to content
+			minScale: 0.2,
+			maxScale: 2.0,
+
+			// Display controls in the bottom right corner
+			controls: true,
+
+			// Display a presentation progress bar
+			progress: true,
+
+			// Display the page number of the current slide
+			slideNumber: false,
+
+			// Determine which displays to show the slide number on
+			showSlideNumber: 'all',
+
+			// Push each slide change to the browser history
+			history: false,
+
