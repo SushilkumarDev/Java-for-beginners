24ea968483285bf3440c5a818a2b7373d135ed9b
SushilkumarDev <sushilkumardeveloper@gmail.com>
2024-05-04 17:50:44 +0530

    Add reveal.js library with licensing information

---
+++ 12-Collections/js/reveal.js
@@ -0,0 +1,21 @@
+/*!
+ * reveal.js
+ * http://lab.hakim.se/reveal-js
+ * MIT licensed
+ *
+ * Copyright (C) 2017 Hakim El Hattab, http://hakim.se
+ */
+(function( root, factory ) {
+	if( typeof define === 'function' && define.amd ) {
+		// AMD. Register as an anonymous module.
+		define( function() {
+			root.Reveal = factory();
+			return root.Reveal;
+		} );
+	} else if( typeof exports === 'object' ) {
+		// Node. Does not work with strict CommonJS.
+		module.exports = factory();
+	} else {
+		// Browser globals.
+		root.Reveal = factory();
+	}
