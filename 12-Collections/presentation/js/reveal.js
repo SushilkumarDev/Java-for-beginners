3ead409273f5047db5382d46389b2ed5f51a996f
SushilkumarDev <sushilkumardeveloper@gmail.com>
2024-05-05 10:29:21 +0530

    Added support for parallax background and corresponding CSS transitions

--- 12-Collections/presentation/js/reveal.js
+++ 12-Collections/presentation/js/reveal.js
@@ -823,3 +823,26 @@
 				} );
 	
 			} );
+					// Add parallax background if specified
+		if( config.parallaxBackgroundImage ) {
+
+			dom.background.style.backgroundImage = 'url("' + config.parallaxBackgroundImage + '")';
+			dom.background.style.backgroundSize = config.parallaxBackgroundSize;
+
+			// Make sure the below properties are set on the element - these properties are
+			// needed for proper transitions to be set on the element via CSS. To remove
+			// annoying background slide-in effect when the presentation starts, apply
+			// these properties after short time delay
+			setTimeout( function() {
+				dom.wrapper.classList.add( 'has-parallax-background' );
+			}, 1 );
+
+		}
+		else {
+
+			dom.background.style.backgroundImage = '';
+			dom.wrapper.classList.remove( 'has-parallax-background' );
+
+		}
+
+	}
