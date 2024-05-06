04ff6bdacef0cbd2fce81d1fad32b33a555424cc
SushilkumarDev <sushilkumardeveloper@gmail.com>
2024-05-06 22:57:04 +0530

    revel.js file updated

--- 12-Collections/presentation/js/reveal.js
+++ 12-Collections/presentation/js/reveal.js
@@ -1720,4 +1720,37 @@ function transformSlides( transforms ) {
 		}
 
 	}
+/**
+	 * Applies layout logic to the contents of all slides in
+	 * the presentation.
+	 *
+	 * @param {string|number} width
+	 * @param {string|number} height
+	 */
+function layoutSlideContents( width, height ) {
 
+	// Handle sizing of elements with the 'stretch' class
+	toArray( dom.slides.querySelectorAll( 'section > .stretch' ) ).forEach( function( element ) {
+
+		// Determine how much vertical space we can use
+		var remainingHeight = getRemainingHeight( element, height );
+
+		// Consider the aspect ratio of media elements
+		if( /(img|video)/gi.test( element.nodeName ) ) {
+			var nw = element.naturalWidth || element.videoWidth,
+				nh = element.naturalHeight || element.videoHeight;
+
+			var es = Math.min( width / nw, remainingHeight / nh );
+
+			element.style.width = ( nw * es ) + 'px';
+			element.style.height = ( nh * es ) + 'px';
+
+		}
+		else {
+			element.style.width = width + 'px';
+			element.style.height = remainingHeight + 'px';
+		}
+
+	} );
+
+}
