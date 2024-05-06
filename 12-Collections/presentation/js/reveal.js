c1601a89714b50e328b05d430d84e98c120c7729
SushilkumarDev <sushilkumardeveloper@gmail.com>
2024-05-06 22:48:21 +0530

    revel.js file updated

--- 12-Collections/presentation/js/reveal.js
+++ 12-Collections/presentation/js/reveal.js
@@ -1505,3 +1505,17 @@ function transformSlides( transforms ) {
 	 * Dispatches an event of the specified type from the
 	 * reveal DOM element.
 	 */
+	function dispatchEvent( type, args ) {
+
+		var event = document.createEvent( 'HTMLEvents', 1, 2 );
+		event.initEvent( type, true, true );
+		extend( event, args );
+		dom.wrapper.dispatchEvent( event );
+
+		// If we're in an iframe, post each reveal.js event to the
+		// parent window. Used by the notes plugin
+		if( config.postMessageEvents && window.parent !== window.self ) {
+			window.parent.postMessage( JSON.stringify({ namespace: 'reveal', eventName: type, state: getState() }), '*' );
+		}
+
+	}
