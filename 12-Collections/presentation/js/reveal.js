cb3fa003c94991d665e60399c75a37c83336dbcc
SushilkumarDev <sushilkumardeveloper@gmail.com>
2024-05-08 00:00:09 +0530

    revel.js file updated

--- 12-Collections/presentation/js/reveal.js
+++ 12-Collections/presentation/js/reveal.js
@@ -2029,3 +2029,11 @@ function isOverview() {
  * orientation of
  * @return {Boolean}
  */
+function isVerticalSlide( slide ) {
+
+	// Prefer slide argument, otherwise use current slide
+	slide = slide ? slide : currentSlide;
+
+	return slide && slide.parentNode && !!slide.parentNode.nodeName.match( /section/i );
+
+}
