22ba5c16ed99be5ba47c19fc6fb7b9f7195b721a
SushilkumarDev <sushilkumardeveloper@gmail.com>
2024-05-07 23:59:02 +0530

    revel.js file updated

--- 12-Collections/presentation/js/reveal.js
+++ 12-Collections/presentation/js/reveal.js
@@ -1937,3 +1937,35 @@ function updateOverview() {
 	} );
 
 }
+/**
+	 * Exits the slide overview and enters the currently
+	 * active slide.
+	 */
+function deactivateOverview() {
+
+	// Only proceed if enabled in config
+	if( config.overview ) {
+
+		overview = false;
+
+		dom.wrapper.classList.remove( 'overview' );
+		dom.wrapper.classList.remove( 'overview-animated' );
+
+		// Temporarily add a class so that transitions can do different things
+		// depending on whether they are exiting/entering overview, or just
+		// moving from slide to slide
+		dom.wrapper.classList.add( 'overview-deactivating' );
+
+		setTimeout( function () {
+			dom.wrapper.classList.remove( 'overview-deactivating' );
+		}, 1 );
+
+		// Move the background element back out
+		dom.wrapper.appendChild( dom.background );
+
+		// Clean up changes made to slides
+		toArray( dom.wrapper.querySelectorAll( SLIDES_SELECTOR ) ).forEach( function( slide ) {
+			transformElement( slide, '' );
+
+			slide.removeEventListener( 'click', onOverviewSlideClicked, true );
+		} );
