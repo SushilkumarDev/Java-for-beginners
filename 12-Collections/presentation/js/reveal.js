d251d30ab204b19e92c60182fcef191b8cfe584a
SushilkumarDev <sushilkumardeveloper@gmail.com>
2024-05-10 22:54:31 +0530

    reveal.js is updated

--- 12-Collections/presentation/js/reveal.js
+++ 12-Collections/presentation/js/reveal.js
@@ -2622,4 +2622,46 @@ return index;
 			}
 	
 		}
+		/**
+	 * Updates the slide number div to reflect the current slide.
+	 *
+	 * The following slide number formats are available:
+	 *  "h.v":	horizontal . vertical slide number (default)
+	 *  "h/v":	horizontal / vertical slide number
+	 *    "c":	flattened slide number
+	 *  "c/t":	flattened slide number / total slides
+	 */
+		function updateSlideNumber() {
+
+			// Update slide number if enabled
+			if( config.slideNumber && dom.slideNumber ) {
+	
+				var value = [];
+				var format = 'h.v';
+	
+				// Check if a custom number format is available
+				if( typeof config.slideNumber === 'string' ) {
+					format = config.slideNumber;
+				}
+	
+				switch( format ) {
+					case 'c':
+						value.push( getSlidePastCount() + 1 );
+						break;
+					case 'c/t':
+						value.push( getSlidePastCount() + 1, '/', getTotalSlides() );
+						break;
+					case 'h/v':
+						value.push( indexh + 1 );
+						if( isVerticalSlide() ) value.push( '/', indexv + 1 );
+						break;
+					default:
+						value.push( indexh + 1 );
+						if( isVerticalSlide() ) value.push( '.', indexv + 1 );
+				}
+	
+				dom.slideNumber.innerHTML = formatSlideNumber( value[0], value[1], value[2] );
+			}
+	
+		}
 	
