8c927ce4997afcb191b8b4d424682537f122f6c8
SushilkumarDev <sushilkumardeveloper@gmail.com>
2024-05-10 22:49:43 +0530

    reveal.js is updated

--- 12-Collections/presentation/js/reveal.js
+++ 12-Collections/presentation/js/reveal.js
@@ -2343,3 +2343,27 @@ function sync() {
 	}
 
 }
+/**
+	 * Resets all vertical slides so that only the first
+	 * is visible.
+	 */
+function resetVerticalSlides() {
+
+	var horizontalSlides = toArray( dom.wrapper.querySelectorAll( HORIZONTAL_SLIDES_SELECTOR ) );
+	horizontalSlides.forEach( function( horizontalSlide ) {
+
+		var verticalSlides = toArray( horizontalSlide.querySelectorAll( 'section' ) );
+		verticalSlides.forEach( function( verticalSlide, y ) {
+
+			if( y > 0 ) {
+				verticalSlide.classList.remove( 'present' );
+				verticalSlide.classList.remove( 'past' );
+				verticalSlide.classList.add( 'future' );
+				verticalSlide.setAttribute( 'aria-hidden', 'true' );
+			}
+
+		} );
+
+	} );
+
+}
