74c65cb09959e3634337341208faef869b749f43
SushilkumarDev <sushilkumardeveloper@gmail.com>
2024-05-11 21:06:52 +0530

    reveal.js is updated

--- 12-Collections/presentation/js/reveal.js
+++ 12-Collections/presentation/js/reveal.js
@@ -3600,4 +3600,31 @@ return index;
 			return horizontalBackground;
 	
 		}
+		/**
+	 * Retrieves the speaker notes from a slide. Notes can be
+	 * defined in two ways:
+	 * 1. As a data-notes attribute on the slide <section>
+	 * 2. As an <aside class="notes"> inside of the slide
+	 *
+	 * @param {HTMLElement} [slide=currentSlide]
+	 * @return {(string|null)}
+	 */
+		function getSlideNotes( slide ) {
+
+			// Default to the current slide
+			slide = slide || currentSlide;
 	
+			// Notes can be specified via the data-notes attribute...
+			if( slide.hasAttribute( 'data-notes' ) ) {
+				return slide.getAttribute( 'data-notes' );
+			}
+	
+			// ... or using an <aside class="notes"> element
+			var notesElement = slide.querySelector( 'aside.notes' );
+			if( notesElement ) {
+				return notesElement.innerHTML;
+			}
+	
+			return null;
+	
+		}
