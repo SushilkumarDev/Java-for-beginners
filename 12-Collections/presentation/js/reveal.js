4770edae254c71547d3cd3d647cfc9ab56deeaaf
SushilkumarDev <sushilkumardeveloper@gmail.com>
2024-05-11 21:03:38 +0530

    reveal.js is updated

--- 12-Collections/presentation/js/reveal.js
+++ 12-Collections/presentation/js/reveal.js
@@ -3434,3 +3434,46 @@ return index;
 		}
 
 	}
+		/**
+	 * Updates the page URL (hash) to reflect the current
+	 * state.
+	 *
+	 * @param {number} delay The time in ms to wait before
+	 * writing the hash
+	 */
+		function writeURL( delay ) {
+
+			if( config.history ) {
+	
+				// Make sure there's never more than one timeout running
+				clearTimeout( writeURLTimeout );
+	
+				// If a delay is specified, timeout this call
+				if( typeof delay === 'number' ) {
+					writeURLTimeout = setTimeout( writeURL, delay );
+				}
+				else if( currentSlide ) {
+					var url = '/';
+	
+					// Attempt to create a named link based on the slide's ID
+					var id = currentSlide.getAttribute( 'id' );
+					if( id ) {
+						id = id.replace( /[^a-zA-Z0-9\-\_\:\.]/g, '' );
+					}
+	
+					// If the current slide has an ID, use that as a named link
+					if( typeof id === 'string' && id.length ) {
+						url = '/' + id;
+					}
+					// Otherwise use the /h/v index
+					else {
+						if( indexh > 0 || indexv > 0 ) url += indexh;
+						if( indexv > 0 ) url += '/' + indexv;
+					}
+	
+					window.location.hash = url;
+				}
+			}
+	
+		}
+	
