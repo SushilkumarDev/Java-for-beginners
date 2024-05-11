6e80f30d1df7e49143fe66d89b20197a0dbe61d4
SushilkumarDev <sushilkumardeveloper@gmail.com>
2024-05-11 21:09:06 +0530

    reveal.js is updated

--- 12-Collections/presentation/js/reveal.js
+++ 12-Collections/presentation/js/reveal.js
@@ -3712,3 +3712,25 @@ function sortFragments( fragments ) {
 				unordered.push( [ fragment ] );
 			}
 		} );
+		// Append fragments without explicit indices in their
+		// DOM order
+		ordered = ordered.concat( unordered );
+
+		// Manually count the index up per group to ensure there
+		// are no gaps
+		var index = 0;
+
+		// Push all fragments in their sorted order to an array,
+		// this flattens the groups
+		ordered.forEach( function( group ) {
+			group.forEach( function( fragment ) {
+				sorted.push( fragment );
+				fragment.setAttribute( 'data-fragment-index', index );
+			} );
+
+			index ++;
+		} );
+
+		return sorted;
+
+	}
