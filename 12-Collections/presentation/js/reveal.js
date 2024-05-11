1816bca3289610458c58e5258ce19b38e1e4841d
SushilkumarDev <sushilkumardeveloper@gmail.com>
2024-05-11 21:06:07 +0530

    reveal.js is updated

--- 12-Collections/presentation/js/reveal.js
+++ 12-Collections/presentation/js/reveal.js
@@ -3549,4 +3549,21 @@ return index;
 	
 		}
 	
+			/**
+	 * Returns the slide element matching the specified index.
+	 *
+	 * @return {HTMLElement}
+	 */
+	function getSlide( x, y ) {
+
+		var horizontalSlide = dom.wrapper.querySelectorAll( HORIZONTAL_SLIDES_SELECTOR )[ x ];
+		var verticalSlides = horizontalSlide && horizontalSlide.querySelectorAll( 'section' );
+
+		if( verticalSlides && verticalSlides.length && typeof y === 'number' ) {
+			return verticalSlides ? verticalSlides[ y ] : undefined;
+		}
+
+		return horizontalSlide;
+
+	}
 	
