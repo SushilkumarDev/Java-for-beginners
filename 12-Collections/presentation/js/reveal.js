b0507e8f928ee176d3767d796acab7f4c8438925
SushilkumarDev <sushilkumardeveloper@gmail.com>
2024-05-11 21:01:30 +0530

    reveal.js is updated

--- 12-Collections/presentation/js/reveal.js
+++ 12-Collections/presentation/js/reveal.js
@@ -3207,3 +3207,47 @@ return index;
 		event.target.removeEventListener( 'loadeddata', startEmbeddedMedia );
 
 	}
+		/**
+	 * "Starts" the content of an embedded iframe using the
+	 * postMessage API.
+	 *
+	 * @param {object} event
+	 */
+		function startEmbeddedIframe( event ) {
+
+			var iframe = event.target;
+	
+			if( iframe && iframe.contentWindow ) {
+	
+				var isAttachedToDOM = !!closestParent( event.target, 'html' ),
+					isVisible  		= !!closestParent( event.target, '.present' );
+	
+				if( isAttachedToDOM && isVisible ) {
+	
+					// Prefer an explicit global autoplay setting
+					var autoplay = config.autoPlayMedia;
+	
+					// If no global setting is available, fall back on the element's
+					// own autoplay setting
+					if( typeof autoplay !== 'boolean' ) {
+						autoplay = iframe.hasAttribute( 'data-autoplay' ) || !!closestParent( iframe, '.slide-background' );
+					}
+	
+					// YouTube postMessage API
+					if( /youtube\.com\/embed\//.test( iframe.getAttribute( 'src' ) ) && autoplay ) {
+						iframe.contentWindow.postMessage( '{"event":"command","func":"playVideo","args":""}', '*' );
+					}
+					// Vimeo postMessage API
+					else if( /player\.vimeo\.com\//.test( iframe.getAttribute( 'src' ) ) && autoplay ) {
+						iframe.contentWindow.postMessage( '{"method":"play"}', '*' );
+					}
+					// Generic postMessage API
+					else {
+						iframe.contentWindow.postMessage( 'slide:start', '*' );
+					}
+	
+				}
+	
+			}
+	
+		}
