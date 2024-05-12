6fbd691258aa7421352c955821e6813fe4eba957
SushilkumarDev <sushilkumardeveloper@gmail.com>
2024-05-12 21:39:22 +0530

    Add onOverviewSlideClicked function to handle clicks on slides in overview mode. It deactivates the overview and navigates to the clicked slide.

--- 12-Collections/presentation/js/reveal.js
+++ 12-Collections/presentation/js/reveal.js
@@ -4485,4 +4485,36 @@ function isSwipePrevented( target ) {
 		}
 
 	}
+	/**
+	 * Invoked when a slide is and we're in the overview.
+	 *
+	 * @param {object} event
+	 */
+	function onOverviewSlideClicked( event ) {
 
+		// TODO There's a bug here where the event listeners are not
+		// removed after deactivating the overview.
+		if( eventsAreBound && isOverview() ) {
+			event.preventDefault();
+
+			var element = event.target;
+
+			while( element && !element.nodeName.match( /section/gi ) ) {
+				element = element.parentNode;
+			}
+
+			if( element && !element.classList.contains( 'disabled' ) ) {
+
+				deactivateOverview();
+
+				if( element.nodeName.match( /section/gi ) ) {
+					var h = parseInt( element.getAttribute( 'data-index-h' ), 10 ),
+						v = parseInt( element.getAttribute( 'data-index-v' ), 10 );
+
+					slide( h, v );
+				}
+
+			}
+		}
+
+	}
